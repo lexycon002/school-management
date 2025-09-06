7c9e8e76fe674b56cf87e8c32241b6bfb5b20fca
Hammad Awowole <lexycon002@gmail.com>
2025-09-06 07:54:55 +0100

    first commit

--- src/app/(dashboard)/list/students/[id]/page.tsx
+++ src/app/(dashboard)/list/students/[id]/page.tsx
@@ -201,7 +201,6 @@
 // export default SingleStudentPage;
 
 
-
 import Announcements from "@/components/Announcements";
 import BigCalendarContainer from "@/components/BigCalendarContainer";
 import FormContainer from "@/components/FormContainer";
@@ -216,7 +215,9 @@ import { notFound } from "next/navigation";
 import { Suspense } from "react";
 
 interface SingleStudentPageProps {
-  params: { id: string };
+  params: {
+    id: string;
+  };
 }
 
 const SingleStudentPage = async ({ params }: SingleStudentPageProps) => {
@@ -246,7 +247,7 @@ const SingleStudentPage = async ({ params }: SingleStudentPageProps) => {
       <div className="w-full xl:w-2/3">
         {/* Top */}
         <div className="flex flex-col lg:flex-row gap-4">
-          {/* Usercard info */}
+          {/* User Card */}
           <div className="bg-secondary py-6 px-4 rounded-md flex-1 flex gap-4">
             <div className="w-1/3">
               <Image
@@ -267,7 +268,7 @@ const SingleStudentPage = async ({ params }: SingleStudentPageProps) => {
                 )}
               </div>
               <p className="text-sm text-gray-500">
-                A computer Science Student.
+                A computer science student.
               </p>
               <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
@@ -291,9 +292,10 @@ const SingleStudentPage = async ({ params }: SingleStudentPageProps) => {
               </div>
             </div>
           </div>
-          {/* Small cards */}
+
+          {/* Small Cards */}
           <div className="flex-1 flex gap-4 justify-between flex-wrap">
-            {/* Attendance */}
+            {/* Attendance Card */}
             <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
               <Image
                 src="/singleAttendance.png"
@@ -306,7 +308,7 @@ const SingleStudentPage = async ({ params }: SingleStudentPageProps) => {
                 <StudentAttendanceCard id={student.id} />
               </Suspense>
             </div>
-            {/* Grade */}
+            {/* Grade Card */}
             <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
               <Image
                 src="/singleBranch.png"
@@ -322,7 +324,7 @@ const SingleStudentPage = async ({ params }: SingleStudentPageProps) => {
                 <span className="text-sm text-gray-400">Grade</span>
               </div>
             </div>
-            {/* Lessons */}
+            {/* Lessons Card */}
             <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
               <Image
                 src="/singleLesson.png"
@@ -338,7 +340,7 @@ const SingleStudentPage = async ({ params }: SingleStudentPageProps) => {
                 <span className="text-sm text-gray-400">Lessons</span>
               </div>
             </div>
-            {/* Class */}
+            {/* Class Card */}
             <div className="bg-white p-4 rounded-md flex gap-4 w-full md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
               <Image
                 src="/singleClass.png"
@@ -354,12 +356,14 @@ const SingleStudentPage = async ({ params }: SingleStudentPageProps) => {
             </div>
           </div>
         </div>
+
         {/* Bottom */}
         <div className="mt-4 bg-white rounded-md p-4 h-[800px]">
           <h1>Student&apos;s Schedule</h1>
           <BigCalendarContainer type="classId" id={student.class.id} />
         </div>
       </div>
+
       {/* Right */}
       <div className="w-full xl:w-1/3 flex flex-col gap-4">
         <div className="bg-white p-4 rounded-md">
@@ -405,3 +409,4 @@ const SingleStudentPage = async ({ params }: SingleStudentPageProps) => {
 };
 
 export default SingleStudentPage;
+
